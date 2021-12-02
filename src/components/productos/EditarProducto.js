4eba5075a15b6a8e263bfd73ec1fad2cf648231c
Fermin Aguilar <ferminab99@gmail.com>
2021-12-02 17:45:39 -0300

    se agrego la logica para editarProducto y se hizo la peticion PUT

--- src/components/productos/EditarProducto.js
+++ src/components/productos/EditarProducto.js
@@ -1,60 +1,125 @@
-import React, { useEffect, useState } from 'react';
-import { Form,Button } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
-const EditarProducto = () => {
-const {id} = useParams();
-const [producto, setProducto] = useState({});
-const URL = process.env.REACT_APP_API_URL + "/" + id;
+import React, { useEffect, useRef, useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import { campoRequerido, rangoPrecio } from "../helpers/helpers";
 
-useEffect(async()=>{
-  // constular un solo elemnto a la api
-  try{
-    const respuesta = await fetch(URL);
-    if (respuesta.status === 200){
-      const dato = await respuesta.json();
-      setProducto(dato);
+const EditarProducto = (props) => {
+  const { id } = useParams();
+  const [producto, setProducto] = useState({});
+  const [categoria, setCategoria] = useState("");
+  const URL = process.env.REACT_APP_API_URL + "/" + id;
+  // crear variables de referencia
+
+  const nombreProductoRef = useRef('');
+  const precioProductoRef = useRef(0); 
+  useEffect(async () => {
+    // constular un solo elemnto a la api
+    try {
+      const respuesta = await fetch(URL);
+      if (respuesta.status === 200) {
+        const dato = await respuesta.json();
+        setProducto(dato);
+        setCategoria(dato.categoria);
+      }
+    } catch (error) {
+      // mostrar mensaje de error
+      console.log(error);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    // validar los datos
+    if (campoRequerido(nombreProductoRef.current.value)&&campoRequerido(categoria)&&rangoPrecio(precioProductoRef.current.value)){
+      console.log('todo ok pa');
+      // crear un objeto y enviarlo a la api
+      const productoModificado = {
+        nombreProducto : nombreProductoRef.current.value,
+        precioProducto : precioProductoRef.current.value,
+        categoria : categoria
+      }
+      // pedir modificar datos a la API, peticion PUT
+      try{
+        const respuesta = await fetch (URL, {
+          method: 'PUT',
+          headers:{'Content-Type':'application/json' },
+          body: JSON.stringify(productoModificado)
+        })
+        
+        if(respuesta.status===200){
+          Swal.fire(
+            'Producto modificado',
+            'El producto de correctamente actualizado',
+            'success'
+          )
+          props.consultarAPI();
+        }
+      }catch(error){
+        console.log(error); 
+        // mostrar mensaje al usuario
+
+      }
+
+
+    }else{
+      console.log("datos invalidos");
+      // mostrar mensaje de error
+
     }
 
-  }catch(error){
-    // mostrar mensaje de error
+    
   }
 
-},[]);
+  return (
+    <div className="container">
+      <section>
+        <h1 className="display-3 my-4 text-center">Editar Producto</h1>
+        <hr />
+      </section>
+      <section className="my-3">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Label>Nombre del producto*</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ej: Café"
+              defaultValue={producto.nombreProducto}
+              ref={nombreProductoRef}
+            />
+          </Form.Group>
 
-
-    return (
-        <div className="container">
-        <section>
-          <h1 className="display-3 my-4 text-center">Editar Producto</h1>
-          <hr />
-        </section>
-        <section className="my-3">
-          <Form>
-            <Form.Group className="mb-3">
-              <Form.Label>Nombre del producto*</Form.Label>
-              <Form.Control type="text" placeholder="Ej: Café" />
-            </Form.Group>
-  
-            <Form.Group className="mb-3">
-              <Form.Label>Precio*</Form.Label>
-              <Form.Control type="text" placeholder="Ej: 50" />
-            </Form.Group>
-            <Form.Group className='mb-3'>
+          <Form.Group className="mb-3">
+            <Form.Label>Precio*</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ej: 50"
+              defaultValue={producto.precioProducto}
+              ref={precioProductoRef}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
             <Form.Label>Categoria*</Form.Label>
-            <Form.Select aria-label="Default select example" >
-              <option>Seleccione una opcion</option>
-              <option value="1">Opcion 1</option>
-              <option value="2">Opcion 2</option>
-              <option value="3">Opcion 3</option>
+            <Form.Select
+              value={categoria}
+              onChange={(e) => setCategoria(e.target.value)}
+            >
+              <option value="">Seleccione una opcion</option>
+              <option value="bebida-caliente">Bebida Caliente</option>
+              <option value="bebida-fria">Bebida Fria</option>
+              <option value="sandwich">Sandwich</option>
+              <option value="dulce">Dulce</option>
+              <option value="salado">Salado</option>
             </Form.Select>
-            </Form.Group>
-            <Button variant="primary" type="submit" className='w-100'>
-              Guardar
-            </Button>
-          </Form>
-        </section>
-      </div>
-    );
+          </Form.Group>
+          <Button variant="primary" type="submit" className="w-100">
+            Guardar
+          </Button>
+        </Form>
+      </section>
+    </div>
+  );
 };
 
 export default EditarProducto;
